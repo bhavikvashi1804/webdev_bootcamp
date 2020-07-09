60e6f02f800e52d93804abf20169250c321b7fd5
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-07-09 18:07:13 +0530

    Play Audio

--- Drum Kit/js/index.js
+++ Drum Kit/js/index.js
@@ -2,6 +2,7 @@ var noOfDrumButton=document.querySelectorAll(".drum").length;
 
 for(var i=0;i<noOfDrumButton;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function (){
-        alert("I got clicked");
+        var audio=new Audio('./sounds/tom-1.mp3');
+        audio.play();
     });
 }
