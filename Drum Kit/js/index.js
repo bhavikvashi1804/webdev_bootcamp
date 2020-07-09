7e413bbf78d4b8a5abe5dfd64243bca4ea109530
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-07-09 17:53:33 +0530

    other way to add event listener

--- Drum Kit/js/index.js
+++ Drum Kit/js/index.js
@@ -1,7 +1,3 @@
-document.querySelector("button").addEventListener("click",handleClick);
-//this is for 1st button
-
-
-function handleClick(){
+document.querySelector("button").addEventListener("click",function (){
     alert("I got clicked");
-}
+});
