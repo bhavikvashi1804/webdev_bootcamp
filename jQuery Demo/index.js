7efed08810be746b2d51a2484a016f101cf24efd
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-07-09 19:51:01 +0530

    CSS & jQuery

--- jQuery Demo/index.js
+++ jQuery Demo/index.js
@@ -3,6 +3,11 @@ console.log("This is working");
 
 $(document).ready(function(){
     $("h1").css("color","red");
+    $("h1").css("font-size","3rem");
+
+    $("h2").addClass("big-title");
+    $("h3").addClass("big-title margin-50");
+    //this will add big title + margin
 });
 
 
