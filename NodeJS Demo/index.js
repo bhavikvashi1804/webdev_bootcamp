6d7ab651a0f2dca8130c6a7182a9e867c0168cb4
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-07-09 23:43:19 +0530

    Use the external package

--- NodeJS Demo/index.js
+++ NodeJS Demo/index.js
@@ -1,2 +1,9 @@
-const fs=require("fs");
-fs.copyFileSync("file1.txt","file2.txt");
+var marvel = require('marvel-characters')
+ 
+// random character
+console.log(marvel())
+//=> 'Iron Man'
+ 
+// all characters
+console.log(marvel.characters)
+//=> ["3-D Man", "A-Bomb", ..., "Zuras", "Zzzax"]
