fd5616d784da9b54f08aa23e1964f04a34b1f585
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-07-09 19:57:17 +0530

    Text & jQuery

--- jQuery Demo/index.js
+++ jQuery Demo/index.js
@@ -2,12 +2,10 @@ console.log("This is working");
 
 
 $(document).ready(function(){
-    $("h1").css("color","red");
-    $("h1").css("font-size","3rem");
+    $("h1").text("Good bye");
 
-    $("h2").addClass("big-title");
-    $("h3").addClass("big-title margin-50");
-    //this will add big title + margin
+    $("button").html("<em>Hello</em>");
+   
 });
 
 
