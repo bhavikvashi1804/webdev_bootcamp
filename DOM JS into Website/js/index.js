c45c1905933bd3bd3aaa02f42c4927425fba68b4
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-07-09 14:45:11 +0530

    Select HTML element Tag Name

--- DOM JS into Website/js/index.js
+++ DOM JS into Website/js/index.js
@@ -3,3 +3,13 @@
 function myFunction(){
     document.getElementById("demo").innerHTML="Good Night";
 }
+
+
+function myFunction1(){
+    var x=document.getElementsByTagName("p");
+
+    for(var i=0;i<x.length;i++){
+        x[i].innerHTML="This is changed by Tag name";
+
+    }
+}
