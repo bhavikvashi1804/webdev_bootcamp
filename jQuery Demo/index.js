2474d5edb5974c9a38bd73cb9fe39caeeae28cb3
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-07-09 20:49:36 +0530

    Animation & jQuery

--- jQuery Demo/index.js
+++ jQuery Demo/index.js
@@ -3,36 +3,13 @@ console.log("This is working");
 
 $(document).ready(function(){
    
-    $("h1").click(function(){
-        $("h1").css("color","red");
-
-    });
-
-
     $("button").click(function(){
-        $("h1").css("color","yellow");
-
+        $("h1").slideUp().slideDown().animate({
+            margin:"20%"
+        });   
     });
 
 
-    //only for input type
-    $("input").keypress(function(event){
-        console.log((event.key));
-    });
-
-    //for whole page
-    $(document).keypress(function(event){
-        console.log((event.key));
-    });
-
-
-    $("h1").on("mouseover",function(){
-        $("h1").css("color","red");
-    });
    
 });
 
-
-//$("h1");
-//$("button");
-//here it selects all buttons 
