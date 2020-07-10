95e105d88cacc2450dfbc1f62e1b44a6c0221650
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-07-10 10:47:31 +0530

    You get the result

--- API Demo/app.js
+++ API Demo/app.js
@@ -8,7 +8,11 @@ app.get('/',function(req,res){
 
     var url="https://api.openweathermap.org/data/2.5/weather?q=Surat&appid=60223a0a2655f6c1379ff1b548eefaa6&units=metric";
     https.get(url,function(response){
-        console.log(response);
+        console.log(response.statusCode);
+
+        response.on("data",function(data){
+            console.log(data);
+        });
     });
     res.send("Server is runing")
 });
