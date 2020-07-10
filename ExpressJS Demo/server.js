0b00a1bb49dfafa02c8ed8541d02e35243a325c5
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-07-10 09:38:00 +0530

    Create html file for calculator

--- ExpressJS Demo/server.js
+++ ExpressJS Demo/server.js
@@ -4,27 +4,9 @@ const app= express();
 
 app.get('/',function(request,response){
     //console.log(request);
-    response.send("<h1>Hello</h1>");
+    response.send("Hello");
 });
 
-
-
-app.get('/contact',function(request,response){
-    //console.log(request);
-    response.send("7359794830");
-});
-
-
-app.get('/about',function(request,response){
-    //console.log(request);
-    response.send("Bhavik Vashi");
-});
-
-
-app.get('/location',function(request,response){
-    //console.log(request);
-    response.send("A/8 Janak Vatika Society");
-});
 
 app.listen(3000,function(){
     console.log("Server is running at port 3000");
