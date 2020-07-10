adfed1b9744b0882cf31ddad82d7814c571f2673
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-07-10 09:06:13 +0530

    Print the request

--- ExpressJS Demo/server.js
+++ ExpressJS Demo/server.js
@@ -1,6 +1,11 @@
 const express= require("express");
 const app= express();
 
+
+app.get('/',function(request,response){
+    console.log(request);
+});
+
 app.listen(3000,function(){
     console.log("Server is running at port 3000");
 });
