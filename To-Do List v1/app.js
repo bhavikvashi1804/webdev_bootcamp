ecd6a74af662e0cbe89b1251dddc452c2b410cd3
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-07-10 17:03:11 +0530

    Create server

---
+++ To-Do List v1/app.js
@@ -0,0 +1,16 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+
+
+
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get('/',function(req,res){
+    res.send("Hello");
+});
+
+app.listen(3000,function(){
+    console.log("Server started at 3000 port");
+});
