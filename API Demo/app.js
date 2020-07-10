9e97233ffaee4a73a01ce957d17da05753cba51d
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-07-10 11:37:58 +0530

    Use the form and display the dynamic data

--- API Demo/app.js
+++ API Demo/app.js
@@ -1,12 +1,23 @@
 const express=require("express");
 const https=require("https");
+const bodyParser=require("body-parser");
 
 const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get('/',function(req,res){
+    res.sendFile(__dirname+"/index.html");
+});
 
-    var url="https://api.openweathermap.org/data/2.5/weather?q=Surat&appid=60223a0a2655f6c1379ff1b548eefaa6&units=metric";
+
+app.post('/',function(req,res){
+    var cityName=req.body.cityName;
+    const baseURL="https://api.openweathermap.org/data/2.5/weather?q=";
+   
+    const apiKey="60223a0a2655f6c1379ff1b548eefaa6";
+    const unit="metric";
+    var url=baseURL+cityName+"&appid="+apiKey+"&units="+unit;
     https.get(url,function(response){
         console.log(response.statusCode);
 
@@ -15,22 +26,21 @@ app.get('/',function(req,res){
             const temp=WeatherData.main.temp;
             const weatherDesc=WeatherData.weather[0].description;
             const icon=WeatherData.weather[0].icon;
-            console.log(temp);
-            console.log(weatherDesc);
-
+           
             const imageURL="http://openweathermap.org/img/wn/"+icon+"@2x.png";
-            console.log(imageURL);
-             
-
+           
             res.write("<p>The weather is currently "+weatherDesc+"</p>");
-            res.write("<h1>The temperature in Surat is "+temp+"*C</h1>");
+            res.write("<h1>The temperature in "+cityName+" is "+temp+"*C</h1>");
             res.write("<img src=" + imageURL +">");
             res.send();
         });
     });
-    //res.send("Server is runing")
+
+
 });
 
 app.listen(3000,function(){
     console.log("Server is runing on port 3000.")
 });
+
+
