5401cc23461467972c8349a7fce11e1cb2886375
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-07-10 13:18:16 +0530

    Parse the data

--- SignUp Page/app.js
+++ SignUp Page/app.js
@@ -3,6 +3,7 @@ const bodyParser=require("body-parser");
 const request=require("request");
 const cors = require('cors');
 const path=require('path');
+const e = require("cors");
 
 
 var app=express();
@@ -12,6 +13,14 @@ app.use(express.static("public"));
 
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/signup.html');
+});
+
+app.post('/',function(req,res){
+    var fName=req.body.fName;
+    var lName=req.body.lName;
+    var email=req.body.email;
+    console.log(fName+lName+email);
+
 });
 
 app.listen(3000,function(){
