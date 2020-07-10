21cc24c9a45f15871616c37d480ee98f395a51e7
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-07-10 10:01:33 +0530

    Calculator using Express

--- ExpressJS Demo/server.js
+++ ExpressJS Demo/server.js
@@ -1,5 +1,9 @@
 const express= require("express");
+const bodyParser=require("body-parser");
+const { request } = require("express");
 const app= express();
+//to get the form data
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get('/',function(request,response){
@@ -7,6 +11,21 @@ app.get('/',function(request,response){
     response.sendFile(__dirname+"/index.html");
     //_dirname automatically find the project dir
 });
+
+app.post('/',function(request,response){
+    //console.log(request.body);
+    //response.send("Thank you for your response");
+    var num1=Number(request.body.num1);
+    var num2=Number(request.body.num2);
+    var result=num1+num2;
+
+
+    response.send("The result of calculation is: "+result);
+
+    
+});
+
+
 
 
 app.listen(3000,function(){
