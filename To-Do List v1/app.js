0a4f3094c27185a3fcb3d8727af44f985d09fb96
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-07-11 09:54:36 +0530

    JS date formating

--- To-Do List v1/app.js
+++ To-Do List v1/app.js
@@ -9,40 +9,17 @@ app.set('view engine','ejs');
 
 
 app.get('/',function(req,res){
-   var today=new Date();
-   var currentDay=today.getDay();
-   var day="";
 
-   
-   switch(currentDay){
-        case 0:
-           day="Sunday";
-           break;
-        case 1:
-            day="Monday";
-            break;
-        case 2:
-            day="Tuesday";
-            break;      
-        case 3:
-            day="Wednesday";
-            break;
-        case 4:
-            day="Thrusday";
-            break;
-        case 5:
-            day="Friday";
-            break;
-        case 6:
-            day="Saturday";
-            break;
-        default:
-            day="Error";
-            console.log(currentDay);
-            break;
-   }
+    var today=new Date();
+    var options={
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    };
 
-   res.render('lists', {todayIsDay: day});
+    var day=today.toLocaleDateString("en-US",options);
+
+    res.render('lists', {todayIsDay: day});
 });
 
 app.listen(3000,function(){
