29ca175caaa3ad911a3cf8da893583e3ea3c3106
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-07-11 11:05:00 +0530

    problem when data submit to work it send to home

--- To-Do List v1/app.js
+++ To-Do List v1/app.js
@@ -11,6 +11,7 @@ app.set('view engine','ejs');
 
 
 var item=[];
+var workItems=[];
 
 app.get('/',function(req,res){
 
@@ -25,10 +26,20 @@ app.get('/',function(req,res){
     res.render('lists', {todayIsDay: day,itemArray:item});
 });
 
+app.get('/work',function(req,res){
+     res.render('lists', {todayIsDay: "Work List",itemArray:workItems});
+});
+
 app.post('/',function(req,res){
     var item1=req.body.taskName;
     item.push(item1);
     res.redirect("/");
+});
+
+app.post('/work',function(req,res){
+    var item1=req.body.taskName;
+    workItems.push(item1);
+    res.redirect("/work");
 });
 
 app.listen(3000,function(){
