9b7ab78443fd9a892904152793aea1dd1ab834ae
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-07-11 10:58:14 +0530

    CSS apply

--- To-Do List v1/app.js
+++ To-Do List v1/app.js
@@ -5,6 +5,8 @@ const bodyParser=require("body-parser");
 
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
 app.set('view engine','ejs');
 
 
