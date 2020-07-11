321a29a54e48d8df3ed8a2bd1c2a04bc10288000
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-07-11 09:32:10 +0530

    Task send the day of week

--- To-Do List v1/app.js
+++ To-Do List v1/app.js
@@ -10,14 +10,36 @@ app.set('view engine','ejs');
 
 app.get('/',function(req,res){
    var today=new Date();
-   var currentDay=today.getDate();
+   var currentDay=today.getDay();
    var day="";
 
-   if(today.getDay()===6||today.getDay()===0){
-        day="Weekend";
-   }
-   else{
-       day="Working day";
+   
+   switch(currentDay){
+        case 0:
+           day="Sunday";
+           break;
+        case 1:
+            day="Monday";
+            break;
+        case 2:
+            day="Tuesday";
+            break;      
+        case 3:
+            day="Wednesday";
+            break;
+        case 4:
+            day="Thrusday";
+            break;
+        case 5:
+            day="Friday";
+            break;
+        case 6:
+            day="Saturday";
+            break;
+        default:
+            day="Error";
+            console.log(currentDay);
+            break;
    }
 
    res.render('lists', {todayIsDay: day});
