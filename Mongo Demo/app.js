88d76a301233814177c8cb17a5ab49a4fc37ba03
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-07-13 14:57:11 +0530

    Insert the documents

--- Mongo Demo/app.js
+++ Mongo Demo/app.js
@@ -11,7 +11,53 @@ const client = new MongoClient(url,{useUnifiedTopology:true});
 
 // Use connect method to connect to the server
 client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
-  client.close();
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+
+    const db=client.db('fruitsDB');
+
+    insertDocuments(
+        db,
+        function(){
+            client.close();
+        }
+    );
+    
 });
+
+
+
+const insertDocuments = function(db, callback) {
+    
+    
+    // Get the documents collection
+    const collection = db.collection('fruits');
+    
+    // Insert some document
+    collection.insertMany([
+        {
+            name:'Apple',
+            score:8,
+            review:'Sweet'
+        },
+        {
+            name:'Orange',
+            score:6,
+            review:'Juicy'
+        },
+        {
+            name:'Banana',
+            score:9,
+            review:'Great vitamins'
+        }
+    ], function(err, result) {
+      assert.equal(err, null);
+      //no error
+      assert.equal(3, result.result.n);
+      //must 3 result
+      assert.equal(3, result.ops.length);
+      console.log("Inserted 3 documents into the collection");
+      console.log(result);
+      callback(result);
+    });
+}
