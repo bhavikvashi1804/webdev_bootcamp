9c6c6c60a881d93c181b12b877872e1e01679ca7
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-07-14 10:59:52 +0530

    Chain the methods

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -23,55 +23,56 @@ const postSchema=mongoose.Schema({
 const Post=mongoose.model("Post",postSchema);
 
 
-app.get("/posts",function(req,res){
-  Post.find({},function(error,foundPosts){
-    if(error){
-      //console.log(error);
-      res.send("Error");
-    }
-    else{
-      //console.log(foundPosts);
-      res.send(foundPosts);
-    }
-  });
-});
-
-app.post("/posts",function(req,res){
-  const title=req.body.title;
-  const content=req.body.content;
-
-  const newPost=new Post({
-    title:title,
-    content:content
-  });
-  
-  newPost.save(function(error){
-    if(error){
-      res.send("Error");
-    }
-    else{
-      res.send("New Post Added");
-    }
-  });
-
-});
-
-
-app.delete("/posts",function(req,res){
-  Post.deleteMany(
-    {},
-    function(error){
+app.route('/posts').get(
+  function(req,res){
+    Post.find({},function(error,foundPosts){
       if(error){
-        console.log("Error");
+        //console.log(error);
         res.send("Error");
       }
       else{
-        console.log("Deleted All Posts");
-        res.send("Deleted All Posts");
+        //console.log(foundPosts);
+        res.send(foundPosts);
       }
-    }
-  );
-});
+    });
+  }
+).post(
+  function(req,res){
+    const title=req.body.title;
+    const content=req.body.content;
+  
+    const newPost=new Post({
+      title:title,
+      content:content
+    });
+    
+    newPost.save(function(error){
+      if(error){
+        res.send("Error");
+      }
+      else{
+        res.send("New Post Added");
+      }
+    });
+  }
+).delete(
+  function(req,res){
+    Post.deleteMany(
+      {},
+      function(error){
+        if(error){
+          console.log("Error");
+          res.send("Error");
+        }
+        else{
+          console.log("Deleted All Posts");
+          res.send("Deleted All Posts");
+        }
+      }
+    );
+  }
+);
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
