86faf5849c134006ed355a684f77e61250050bb7
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-07-14 11:56:08 +0530

    handle the request

--- Secrets/app.js
+++ Secrets/app.js
@@ -14,9 +14,15 @@ app.use(express.static("public"));
 
 
 
-
-
-
+app.get("/",function(req,res){
+    res.render("home");
+});
+app.get("/login",function(req,res){
+    res.render("login");
+});
+app.get("/register",function(req,res){
+    res.render("register");
+});
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
